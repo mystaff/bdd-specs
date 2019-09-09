ebb43a57a7acfd908c25143f2190cbe9f94594c5
Hassan Magdy <Hasan.Magdy@gmail.com>
2019-09-09 15:39:59 +0200

    make it CLI

--- script/lib/app.js
+++ script/lib/app.js
@@ -61,29 +61,38 @@ class App {
         let file_name = ar[1];
         var DOWNLOAD_DIR = __dirname + '/downloads/';
         // let file_name = 'TD2.msi';
+        console.log("BEFORE IF");
+        console.log((DOWNLOAD_DIR + file_name));
         console.log(fs.existsSync(DOWNLOAD_DIR + file_name));
         if (!fs.existsSync(DOWNLOAD_DIR + file_name)) {
             // create an instance of writable stream
             var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+            console.log("AFTER createWriteStream");
+            console.log((DOWNLOAD_DIR + file_name));
+            console.log(fs.existsSync(DOWNLOAD_DIR + file_name));
             // execute curl using child_process' spawn function
             // var curl = spawn('curl', [file_url]);
+            console.log('START spawn');
             let curl = spawn('curl', ['-L', file_url])
             // add a 'data' event listener for the spawn instance
             curl.stdout.on('data', function (data) {
                 console.log(chalk.yellow('----------downloading TD2 Silent App------------------ ..please wait .. that might take a while..'));
                 file.write(data);
             });
+            console.log('START spawn 1 if');
             // add an 'end' event listener to close the writeable stream
             curl.stdout.on('end', function (data) {
                 file.end();
                 console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
             });
+            console.log('START spawn 2 if');
             // when the spawn child process exits, check if there were any errors and close the writeable stream
             curl.on('exit', function (code) {
                 if (code != 0) {
                     console.log('Failed: ' + code);
                 }
             });
+            console.log('START spawn 3 if');
         }
 
         let ls = spawn('msiexec', ['/i  /quiet /qn /norestart', DOWNLOAD_DIR + file_name])
